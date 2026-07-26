d344b4b6e20958b69a08377e455a641a4a9cefaa
Niclas <130746231+NicDevTV@users.noreply.github.com>
2026-07-26 13:59:29 +0200

    chore(docker): remove docker enviroment for now

--- docker/down.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-docker compose --file "$PSScriptRoot\compose.yml" down
-
-if ($LASTEXITCODE -ne 0) {
-    throw "Docker Compose shutdown failed with exit code $LASTEXITCODE."
-}
